88b91ea47d7a7529302c44dc48a35bd32fb9416a
Annah Nokutheza Mwanza <110993039+AnnahNokutheza@users.noreply.github.com>
2022-10-17 23:07:36 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,44 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
+
 /**
- * struct forPrintf -> structure contrary functions
- *
- * @operation: the operator
- * @f: function
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
  */
 
-struct forPrintf
+typedef struct specifiers
 {
-	char *operation;
+	char specifiers;
 	int (*f)(va_list);
-};
-typedef struct forPrintf toPrint;
+} specifiers_t;
 
+/*prototypes*/
 int _printf(const char *format, ...);
-int _putchar(char);
-int pchar(va_list);
-int pstr(va_list);
-int percent(va_list);
-int pinteger(va_list);
-int number(va_list);
-int parse(const char *format, toPrint func[], va_list args);
-int unsigned_int(va_list);
-int unsigned_number(unsigned int);
-int print_binary(va_list l);
-int _puts(char *str);
-char *convert(unsigned long int num, int base, int lowercase);
-int print_hex(va_list l);
-int print_hex_big(va_list l);
-int print_octal(va_list l);
-int print_unsigned(va_list l);
-int print_rot13(va_list l);
-int print_rev(va_list l);
-int print_bigS(va_list l);
-int print_address(va_list l);
+int get_function(char s, va_list args);
+int _putchar(char c);
+
+/*Conversion specifiers*/
+int print_char(va_list args);
+int print_string(va_list args);
+int print_digit(va_list args);
+int print_mod(va_list args);
+int print_rev_string(va_list args);
 
 #endif
